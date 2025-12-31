881bfe822f954c1263b2d4f60f8ca742ea82a69e
naren4488 <narendrakajla77@gmail.com>
2025-12-31 22:26:50 +0530

    switched layout for hero section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -19,12 +19,12 @@ export default function HeroSection() {
       <div className="container mx-auto px-4 py-12 md:py-16 lg:py-20 xl:py-24 relative z-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 xl:gap-16 items-center">
           {/* Left Column - Content */}
-          <div className="order-2 lg:order-1">
+          <div className="">
             <HeroContent />
           </div>
 
           {/* Right Column - Visuals */}
-          <div className="order-1 lg:order-2">
+          <div className="">
             <HeroVisuals />
           </div>
         </div>
